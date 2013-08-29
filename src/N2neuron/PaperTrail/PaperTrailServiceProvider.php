075b465021f4a50affdef1258f7a34226fb008a7
Eric Perkins <e.perkins@n2neuron.com>
2013-08-29 14:34:54 -0500

    initial import of blank laravel package

---
+++ src/N2neuron/PaperTrail/PaperTrailServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace N2neuron\PaperTrail;
+
+use Illuminate\Support\ServiceProvider;
+
+class PaperTrailServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('n2neuron/paper-trail');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
